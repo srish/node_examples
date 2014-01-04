0e910002ddb7c6046aa6c616bb3823b2e2363d1e
srishakatux <srishakatux@gmail.com>
2014-01-03 20:59:48 -0500

    Real time interaction with websocket.io

---
+++ mouse_cursors/server.js
@@ -0,0 +1,58 @@
+var express = require('express'),
+	wsio = require('websocket.io')
+
+/* Create express app */ 
+var app = express.createServer();
+
+/* Attach websocket server */
+var ws = wsio.attach(app);
+
+/* Serve your code */
+app.use(express.static('public'));
+
+var positions = {}, 
+	total = 0
+
+/*Listening on connections */
+ws.on('connection', function(socket) {
+	// you give the socket an id
+	console.log("b ksdbfkdsbf kdsjbfjksdb ");
+	socket.id = ++total; 
+
+	//you send the positions of everyone else
+	socket.send(JSON.stringify(positions));
+
+	socket.on('message', function(msg) {
+		try {
+			var pos = JSON.parse(msg);
+		} catch(e) {
+			return;
+		}
+
+		positions[socket.id] = pos;
+		broadcast(JSON.stringify({ type: 'position', pos: pos, id: socket.id}));
+	});
+
+	socket.on('close', function() {
+		delete positions[socket.id];
+		broadcast(JSON.stringify({ type: 'disconnect', 
+			id: socket.id}));
+	});
+
+	function broadcast(msg) {
+		sys.puts(sys.inspect(msg));
+		for(var i = 0, l = ws.clients.length; i< l; i++) {
+			//you avoid sending a message to the same socket that broadcasts
+			if(ws.clients[i] && socket.id != ws.clients[i].id) {
+				//you call 'send' on the other clients
+				ws.clients[i].send(msg);
+			}
+		}
+	}
+	// ...
+});
+
+/* Listen */
+app.listen(3000);
+
+
