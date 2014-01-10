0c441a63022bc21092834662f80487c7c6b8f353
srishakatux <srishakatux@gmail.com>
2014-01-10 02:49:48 -0500

    integrated groove shark api to make this a mini DJ app

--- chat_program/public/chat.js
+++ chat_program/public/chat.js
@@ -20,8 +20,12 @@ window.onload = function() {
 
 	var input = document.getElementById('input');
 	document.getElementById('form').onsubmit = function() {
-		addMessage('me', input.value);
-		socket.emit('text', input.value);
+		var li = addMessage('me', input.value);
+
+		socket.emit('text', input.value, function(date) {
+			li.className = 'confirmed';
+			li.title = date;
+		});
 
 		//reset the input
 		input.value = '';
@@ -32,7 +36,59 @@ window.onload = function() {
 	function addMessage(from, text) {
 		var li = document.createElement('li');
 		li.className = 'message';
-		li.innerHTML = '<b>' + from + '</b>:' + text;
+		li.innerHTML = '<b>' + from + '</b>:' + text ;
 		document.getElementById('messages').appendChild(li);
+		return li;
 	}
+
+	//plays a song 
+	var playing = document.getElementById('playing');
+	function play(song) {
+		if(!song) return;
+		playing.innerHTML = '<hr><b> Now Playing: </b> ' + song.ArtistName + 
+		' ' + song.SongName + '<br>';
+
+		var iframe = document.createElement('iframe');
+		iframe.frameborder = 0;
+		iframe.src = song.Url;
+		playing.appendChild(iframe); 
+	}
+
+	socket.on('song', play);
+
+	//search form 
+	var form = document.getElementById('dj');
+	var results = document.getElementById('results');
+	form.style.display = 'block';
+
+	form.onsubmit = function() {
+		results.innerHTML = '';
+		socket.emit('search', document.getElementById('s').value, function(songs) {
+			for(var i = 0, l = songs.length; i< l; i++) {
+				(function(song) {
+					var result = document.createElement('li');
+					result.innerHTML = song.ArtistName + ' - <b>' + song.SongName 
+					+ '</b>';
+					var a = document.createElement('a');
+					a.href = '#';
+					a.innerHTML = 'Select';
+
+					a.onclick = function() {
+						socket.emit('song', song);
+						play(song);
+						return false;
+					}
+
+					result.appendChild(a);
+					results.appendChild(result);
+				}) (songs[i]);
+			}
+		});
+		
+		return false;
+	};
+
+	socket.on('elected', function() {
+		form.className = 'isDJ';
+	});
 }
--- chat_program/server.js
+++ chat_program/server.js
@@ -1,6 +1,7 @@
 /* Module dependencies */
 var express = require('express'),
-	sio = require('socket.io')
+	sio = require('socket.io'),
+	request = require('superagent')
 
 /* Create app */
 app = express.createServer(
@@ -9,18 +10,66 @@ app = express.createServer(
 );
 
 /* Attach socket.io */
-var io = sio.listen(app);
+var io = sio.listen(app),
+	apiKey = '{ 511efc4655606a43267479ea32e803e9 }',
+	currentSong, dj
+
+/* Elect function does the following 
+   1. Mark the current user as the DJ
+   2. Emit an announcement to everyone that a new DJ is ready
+   3. Let the user know that she had been elected by emitting an elected event.
+   4. Upon the user being disconnected, mark the DJ spot as available so that
+      the next connection becomes the DJ. 
+*/
+
+
+function elect(socket) {
+	dj = socket; 
+	io.sockets.emit('announcement', socket.nickname + ' is the new dj');
+	socket.emit('elected');
+	socket.dj = true;
+	socket.on('disconnect', function() {
+		dj = null;
+		io.sockets.emit('announcement', 'the dj left - next one to join becomes dj');
+	});
+}
 
 /* Set up connection's listener */
 io.sockets.on('connection', function(socket) {
 	//console.log('someone connected');
 	socket.on('join', function(name) {
 		socket.nickname = name;
-		socket.broadcast.emit('announcement', name + ' joined the chat.');
+		socket.broadcast.emit('announcement', socket.nickname + ' joined the chat.');
+	
+		if(!dj) {
+			elect(socket);
+		} else {
+			socket.emit('song', currentSong);
+		}
+
 	});
 
-	socket.on('text', function(msg){
+	
+
+	socket.on('search', function(q, fn) {
+		request('http://tinysong.com/s/' + encodeURIComponent(q) + '?format=json&limit=3&key=511efc4655606a43267479ea32e803e9', function(res) {
+				if(200 == res.status) 
+					fn(JSON.parse(res.text));
+			});
+	});
+
+	socket.on('text', function(msg, fn){
 		socket.broadcast.emit('text', socket.nickname, msg);
+		
+		//confirm the reception
+		fn(Date.now());
+	});
+
+	socket.on('song', function(song) {
+		if(socket.dj) {
+			currentSong = song;
+			socket.broadcast.emit('song', song);
+		}
 	});
 });
 
